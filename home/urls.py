82ce57db426bce18bba24cbaa6237a9bfe7ac3a8
nazarboy2005 <rakhmonovnazarboy0@gmail.com>
2024-12-09 02:16:40 +0300

    templates are ready to serve

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from home.views import HomeView, AboutView, ResumeView
-
+from home.views import HomeView, AboutView, ResumeView, ContactView
 
 app_name = 'home'
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('resume/', ResumeView.as_view(), name='resume'),
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
