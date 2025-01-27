825951c986bea71c692e5b99d7fbc8b1d62eed14
nazarboy2005 <rakhmonovnazarboy0@gmail.com>
2025-01-27 18:05:36 +0300

    everything is almost ready

--- conf/settings.py
+++ conf/settings.py
@@ -76,15 +76,20 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': "django.db.backends.postgresql",
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD':os.getenv('DB_PWD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT':os.getenv('DB_PORT'),
+#     }
+# }
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD':os.getenv('DB_PWD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT':os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 # Password validation
